e229a14e8681aec56d837fe24c402c1d4026d589
true-false-try <6mateo6ch@gmail.com>
2022-09-29 17:38:13 +0300

    created project

---
+++ src/main/java/com/example/demobot/DemobotApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemobotApplication.class, args);
+    }
+
+}
